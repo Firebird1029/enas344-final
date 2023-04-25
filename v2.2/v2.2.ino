1bd634aff25550161116a2090a8a30b21363844c
Brandon Yee <6111102+Firebird1029@users.noreply.github.com>
2023-04-25 00:59:20 -0400

    v2.2 working: integrate OLED_tests, cleanup code

--- v2.2/v2.2.ino
+++ v2.2/v2.2.ino
@@ -1,4 +1,6 @@
+#include <Adafruit_GFX.h>
 #include <Adafruit_MPU6050.h>
+#include <Adafruit_SSD1306.h>
 #include <Adafruit_Sensor.h>
 #include <Audio.h>
 #include <Bounce2.h>
@@ -12,6 +14,16 @@
 // TODO fix VS code errors
 // #include "mixer.h"
 
+// GENERAL
+
+#define LOOP_TIME 2000
+#define MASTER_VOLUME 0.5
+
+// menu encoder sensitivity (how many encoder steps per menu item)
+#define MES 10
+
+#define DELAYLINE_MAX_LEN 441000  // 44100 samples/sec * 10 sec
+
 // PINOUT
 
 #define ENCODER_DT_PIN 3
@@ -22,6 +34,13 @@
 #define LOOP_BTN_PIN 32
 #define COMMIT_BTN_PIN 31
 #define CLEAR_BTN_PIN 30
+
+// OLED
+
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 32
+#define OLED_RESET_PIN -1
+#define SCREEN_ADDRESS 0x3C
 
 // MOTION
 
@@ -42,13 +61,6 @@
 #define GYROX_SCALE 1
 #define GYROY_SCALE 1
 #define GYROZ_SCALE 1
-
-// AUDIO & GENERAL
-
-#define LOOP_TIME 2000
-#define MASTER_VOLUME 0.5
-
-#define DELAYLINE_MAX_LEN 441000  // 44100 samples/sec * 10 sec
 
 // AUDIO
 EXTMEM int16_t DELAY_LINE_TEMP[DELAYLINE_MAX_LEN] = {};
@@ -92,17 +104,20 @@ AudioConnection patchCord17(fullDelay, 0, outMixer, 1);
 AudioControlSGTL5000 sgtl5000_1;  // xy=64.5,20
 // GUItool: end automatically generated code
 
-// ENCODER
-Encoder enc(ENCODER_DT_PIN, ENCODER_CLK_PIN);
-
 // BUTTONS
 // Button loopButton = Button();
 // Button synthButton = Button();
 // Button clearButton = Button();
 Button encButton = Button();
 
-// MPU6050
+// ENCODER
+Encoder enc(ENCODER_DT_PIN, ENCODER_CLK_PIN);
+
+// MPU6050 IMU
 Adafruit_MPU6050 mpu;
+
+// OLED
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET_PIN);
 
 // GLOBAL - MOTION
 
@@ -140,7 +155,7 @@ long unsigned int
 int mappedRibbonPotVal =
     0;  // ribbon softpot values mapped to integer half-tones above fundamental
 
-// STATES
+// GLOBAL - AUDIO STATES
 bool isRecordingLoop = false,  // true if recording loop
     isCommittingLoop = false,  // true if committing loop
     listeningForRoll =
@@ -151,6 +166,27 @@ bool isRecordingLoop = false,  // true if recording loop
 // curSustainStatus == 1 && ribbon pressed -> sustain note
 // curSustainStatus == 1 && ribbon released -> note off, set status := 0
 bool curSustainStatus = false;
+
+// GLOBAL - MENU (OLED + ENCODER)
+
+#define NUM_MODE_OPTIONS 5
+const char *MODE_OPTIONS[NUM_MODE_OPTIONS] = {"Simple", "Soft", "Chiptune",
+                                              "Chord", "Percussion"};
+int modeIndex = 0;
+int clientMasterVolume = 100;
+enum MENU_STATE {
+  MENU_MODE,
+  MENU_VOLUME,
+  MENU_CLEAR,
+  MENU_POWER,
+  MENU_START_LOOP
+};  // MENU_POWER, MENU_START_LOOP unused TODO
+enum MENU_ACTIVE { MENU_INACTIVE, MENU_ACTIVE_OPTION };
+MENU_STATE menuState = MENU_MODE;
+MENU_ACTIVE menuActiveState = MENU_INACTIVE;
+
+int encValue,       // current encoder value
+    savedEncValue;  // encValue of main menu (before entering active option)
 
 void setup(void) {
   // SERIAL SETUP
@@ -212,6 +248,7 @@ void setup(void) {
   // clearButton.interval(5);
   // clearButton.setPressedState(LOW);
 
+  // ENCODER SETUP
   encButton.attach(ENCODER_BTN_PIN, INPUT_PULLUP);
   encButton.interval(5);
   encButton.setPressedState(LOW);
@@ -229,6 +266,19 @@ void setup(void) {
   mpu.setAccelerometerRange(MPU6050_RANGE_2_G);
   mpu.setGyroRange(MPU6050_RANGE_500_DEG);
   mpu.setFilterBandwidth(MPU6050_BAND_184_HZ);
+
+  // OLED SETUP
+  if (!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
+    Serial.println(F("SSD1306 allocation failed"));
+    while (true) {
+      delay(1);
+    }
+  }
+  display.display();
+  delay(500);
+  display.clearDisplay();
+  display.setTextColor(SSD1306_WHITE);
+  display.setTextSize(1);
 
   // DELAY LINES
   tempDelay.begin(DELAY_LINE_TEMP, DELAYLINE_MAX_LEN);
@@ -276,10 +326,6 @@ void loop() {
   // ladder1.frequency(abs(pitch * 1000 + 500));
   // outLadderFreqSine.amplitude(roll / 10);
 
-  // Read encoder data
-  // Serial.println(enc.read());
-  // inWaveformMod.frequency(baseFreq + enc.read());
-
   // Read ribbon pot data
   mappedRibbonPotVal = getRibbonPotValAndMap(0, 12);
   // Serial.println(mappedRibbonPotVal);
@@ -308,11 +354,6 @@ void loop() {
   // loopButton.update();
   // synthButton.update();
   // clearButton.update();
-  encButton.update();
-
-  if (encButton.pressed()) {
-    Serial.println("Encoder button pressed");
-  }
 
   // START RECORDING
   // if (loopButton.pressed() && !isRecordingLoop && !isCommittingLoop) {
@@ -398,6 +439,9 @@ void loop() {
     drumSynth.noteOn();
   }
 
+  // MENU
+  menuCode();
+
   delay(10);  // prevents ramp down bug, switch to timer instead of delay later
 }
 
@@ -436,4 +480,118 @@ int getRibbonPotValAndMap(int newMin, int newMax) {
 float mapFloat(float x, float in_min, float in_max, float out_min,
                float out_max) {
   return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
+// MENU CODE
+
+void menuCode() {
+  // Encoder: Menu Navigation
+  int encValue = enc.read();
+  if (menuActiveState == MENU_INACTIVE) {
+    // encoder toggles through menu options
+    // TODO switch to programmatic way (non if statements)
+    if (encValue >= 0 && encValue < MES) {
+      menuState = MENU_START_LOOP;  // TODO replace with IMU
+    } else if (encValue >= MES && encValue < 2 * MES) {
+      menuState = MENU_CLEAR;
+    } else if (encValue >= 2 * MES && encValue < 3 * MES) {
+      menuState = MENU_MODE;
+    } else if (encValue >= 3 * MES && encValue < 4 * MES) {
+      menuState = MENU_VOLUME;
+      // menuState = MENU_POWER;
+    } else if (encValue < 0) {
+      // TODO switch to programmatic way
+      enc.write(39);
+    } else {
+      enc.write(0);
+    }
+  } else if (menuActiveState == MENU_ACTIVE_OPTION) {
+    if (menuState == MENU_MODE) {
+      // encoder toggles through mode options
+      if (encValue >= MES) {
+        modeIndex = (modeIndex + 1) % NUM_MODE_OPTIONS;
+        enc.write(0);
+      } else if (encValue <= -MES) {
+        modeIndex = (modeIndex + NUM_MODE_OPTIONS - 1) % NUM_MODE_OPTIONS;
+        enc.write(0);
+      }
+    }
+    if (menuState == MENU_VOLUME) {
+      // encoder adjusts volume
+      clientMasterVolume += (int)(encValue / (int)(MES / 5));
+      if (clientMasterVolume < 0) {
+        clientMasterVolume = 0;
+      } else if (clientMasterVolume > 100) {
+        clientMasterVolume = 100;
+      }
+      enc.write(0);
+    }
+  }
+
+  // Encoder Button: Change Active State
+  encButton.update();
+  if (encButton.pressed()) {
+    Serial.println("Encoder button pressed");
+    if (menuActiveState == MENU_INACTIVE) {
+      // change to active option state
+      if (menuState == MENU_START_LOOP || menuState == MENU_CLEAR) {
+        // substate-less menu options
+        if (menuState == MENU_CLEAR) {
+          // clearLoop();
+        }
+
+        // TODO remove MENU_START_LOOP later
+        if (menuState == MENU_START_LOOP) {
+          // startLoop();
+        }
+      } else {
+        savedEncValue = encValue;
+        enc.write(0);
+        menuActiveState = MENU_ACTIVE_OPTION;
+      }
+    } else {
+      // change to inactive (main menu) state
+      encValue = savedEncValue;
+      enc.write(encValue);
+      menuActiveState = MENU_INACTIVE;
+    }
+  }
+
+  display.clearDisplay();
+  display.setCursor(0, 0);
+  // display.println(encValue);
+
+  // Start Loop Recording
+  // TODO remove later
+  displayMenuPrefix(MENU_START_LOOP);
+  display.println("Start Loop");
+
+  // Clear Loop
+  displayMenuPrefix(MENU_CLEAR);
+  display.println("Clear Loop");
+
+  // Mode
+  displayMenuPrefix(MENU_MODE);
+  display.println(MODE_OPTIONS[modeIndex]);
+
+  // Volume
+  displayMenuPrefix(MENU_VOLUME);
+  display.print("Volume: ");
+  display.println(clientMasterVolume);
+
+  display.display();
+}
+
+void displayMenuPrefix(int ms) {
+  if (menuState == ms) {
+    display.print(">");
+  } else {
+    display.print(" ");
+  }
+  if (menuState == ms && menuActiveState == MENU_ACTIVE_OPTION) {
+    display.print(">");
+  } else {
+    display.print(" ");
+  }
+  display.print(" ");
 }
